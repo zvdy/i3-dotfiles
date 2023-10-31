aae98a7e1545067f59b1649f3834cc3f60d583fe
zvdy <cristian.030902@gmail.com>
2023-10-31 16:06:46 +0100

    install script

--- install.py
+++ install.py
@@ -3,12 +3,13 @@
 import os
 
 # Install packages
-os.system("sudo apt-get install -y git vim zsh curl polybar rofi i3 feh kitty neofetch fonts-powerline ")
+os.system("sudo apt-get install -y git zsh curl polybar rofi i3 feh kitty neofetch fonts-powerline")
 
 # Get path and assign it to a variable
 path = os.getcwd()
 
 os.system("cd")
+os.system('sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"')
 os.system("git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ")
 os.system("git clone https://github.com/zsh-users/zsh-autosuggestions.git")
 os.system("git clone https://github.com/zsh-users/zsh-syntax-highlighting.git")
@@ -21,6 +22,11 @@ os.system("cd " + path)
 os.system("cd dotfiles/.config && cp -f -r * ~/.config")
 os.system("cd dotfiles/ && cp -f .zshrc ~ && cp -f .p10k.zsh ~ && cp -f .xsessionrc ~")
 
+# chmod the .xsessionrc file to make it executable and polybar/launch.sh
+os.system("chmod +x ~/.xsessionrc && chmod +x ~/.config/polybar/launch.sh")
+
+# Getting the wallpaper
+os.system("cd dotfiles/wallpapers && cp -f -r * ~/Pictures")
 
 # Download Hack Nerd Font
 os.system("wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Hack.zip")
